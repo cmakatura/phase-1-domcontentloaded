467d575612e3da6205580fac665265bd873bce56
christina makatura <cnmak@comcast.net>
2021-09-20 20:00:33 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newText = document.querySelector('#text').textContent
+    text.textContent = "This is really cool!"
+    console.log(newText) 
+  });
